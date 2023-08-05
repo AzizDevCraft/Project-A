6fbd28aa8adb8e0e5d1060d0a4ec5382dcbc9c0e
aziz <aziz.bengamra@dauphine.tn>
2023-08-05 14:38:55 +0100

    le fichier brouillant.py c'est un fichier pour tester en parallèle mon code et cette version du main c'est presque finit juste il y a des petites imperfections

---
+++ brouillant.py
@@ -0,0 +1,6 @@
+l = [0,"1","5","4","8"]
+print (l)
+l[0] = 'a'
+print (l)
+ch = "".join(l)
+print (ch)
--- main.py
+++ main.py
@@ -7,15 +7,15 @@ with open ("C:/Users/azizb/documents/github_space/Project-A/fichier.txt", "r") a
 x = random.randint (0, len (words)-1) # Pour choisir l'indice d'un mot au hasard 
 word = words [x]
 print ("mot :", word)
-word_cache = "-----"
-print (word_cache) # affichage du mot caché
+print ("-----") # affichage du mot caché
 i = 0 # compteur des tentatives possibles 
 
-while i <= 6 : 
+while i <= 6 :
+    ch = ['-','-','-','-','-'] 
     if i == 6 : 
         print ("c'est votre dernière chance !")
     while True : 
-        essai = input ("Devine le mot :")
+        essai = input ("Devine le mot :").capitalize ()
         try : 
             assert len (essai) == 5 and essai.isalpha () == True 
         except AssertionError : 
@@ -23,8 +23,14 @@ while i <= 6 :
             continue
         else : 
             break 
-    for l in essai : 
-        a = re.search (l, word).span () # erreur dans cette ligne 
-
+    for l in essai :
+        if l in word : 
+            a = re.search (l, word).span ()[0] 
+            ch[a] = l
+    word_cache = "".join (ch)
+    print (word_cache)
+    if word_cache == word : 
+        print ("félicitation! vous avez trouvez le mot.")
+        break 
     i += 1
 
