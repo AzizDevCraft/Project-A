a810cf2ed12a8c339e50a6a19e165d13d246aaae
ryt-odoo <94531686+ryt-odoo@users.noreply.github.com>
2023-08-24 10:25:29 +0100

    Update main.py tried to resolve the existing conflict

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import random
 <<<<<<< HEAD
 import re 
 
-with open ("C:/Users/azizb/documents/github_space/Project-A/fichier.txt", "r") as data :
-    words = data.read().split ("\n")[:-1]
-
-x = random.randint (0, len (words)-1) # Pour choisir l'indice d'un mot au hasard 
-word = words [x]
-print ("mot :", word)
-word_cache = "-----"
-print (word_cache) # affichage du mot caché
-i = 0 # compteur des tentatives possibles 
-
-while i <= 6 : 
-=======
-
 with open ("fichier.txt", "r") as data :
     words = data.read().split ("\n")[:-1]
 
@@ -27,7 +14,6 @@ i = 0 # compteur des tentatives possibles
 
 while i <= 6 :
     ch = ['-','-','-','-','-'] 
->>>>>>> 5368b4b1c2b11859e3c6cd16faa51de74e5544ac
     if i == 6 : 
         print ("c'est votre dernière chance !")
     while True : 
@@ -37,17 +23,11 @@ while i <= 6 :
         except AssertionError : 
             print ("tu peux entrer seulement un mot de 6 lettres seulement !")
             continue
-<<<<<<< HEAD
-        else : 
-            break 
-<<<<<<< HEAD
+        break 
     for l in essai : 
         a = re.search (l, word).span ()
 
-=======
-=======
         break 
->>>>>>> while_adj
     for pos, item in enumerate (essai) :
         if item in word : 
             ch [pos] = item
@@ -60,6 +40,5 @@ while i <= 6 :
         print ("félicitation! vous avez trouvez le mot.")
         break 
     print ("lettre à ne pas utiliser :",alpha)
->>>>>>> 5368b4b1c2b11859e3c6cd16faa51de74e5544ac
     i += 1
 
